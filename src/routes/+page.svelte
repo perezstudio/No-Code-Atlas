fa238b49932a0fdc4cc49956e73856e8aee03ae6
Kevin Perez <kp952547@gmail.com>
2023-12-14 19:43:20 -0700

    defined home page structure

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,18 @@
 <div>
-    <h1 class="text-red-500">Craft Your Future with No Code</h1>
-    <p>Harness the power of No Code Atlas, your key to creating sophisticated tools, building prosperous businesses, and enhancing your everyday life. Experience the brilliance of no code and revolutionize your world!</p>
+    <div>
+        <h1 class="text-red-500">Craft Your Future with No Code</h1>
+        <p>Harness the power of No Code Atlas, your key to creating sophisticated tools, building prosperous businesses, and enhancing your everyday life. Experience the brilliance of no code and revolutionize your world!</p>
+        <a href="#"><p>Test</p></a>
+        <a href="#"><p>Test</p></a>
+    </div>
+</div>
+<div>
+    <div>
+        <div>
+            <p>Compass</p>
+        </div>
+        <h2>Discover the Ideal Tools for Your Vision</h2>
+        <p>Your ultimate guide to discovering the perfect tools for your unique needs. Our intuitive and comprehensive tool finder will match you with the ideal resources to build innovative solutions, launch successful businesses, and streamline your life.</p>
+        <a href="#"><p>Test</p></a>
+    </div>
 </div>
