aabc866253cdfd7c84de6bf474ea3185e5c26f38
Kevin Perez <kp952547@gmail.com>
2023-12-14 19:15:30 -0700

    Update code formatting and fix minor bugs

---
+++ src/routes/+page.svelte
@@ -0,0 +1,4 @@
+<div>
+    <h1>Craft Your Future with No Code</h1>
+    <p>Harness the power of No Code Atlas, your key to creating sophisticated tools, building prosperous businesses, and enhancing your everyday life. Experience the brilliance of no code and revolutionize your world!</p>
+</div>
