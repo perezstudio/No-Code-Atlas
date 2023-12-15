a3974747e7c1f168536312a0d583ea4707a98604
Kevin Perez <kp952547@gmail.com>
2023-12-14 19:27:22 -0700

    Apply and test tailwind styles

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,4 @@
 <div>
-    <h1>Craft Your Future with No Code</h1>
+    <h1 class="text-red-500">Craft Your Future with No Code</h1>
     <p>Harness the power of No Code Atlas, your key to creating sophisticated tools, building prosperous businesses, and enhancing your everyday life. Experience the brilliance of no code and revolutionize your world!</p>
 </div>
