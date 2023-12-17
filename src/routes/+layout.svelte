da26baccf83537b006da418ae18efd8e9d593c79
Kevin Perez <kp952547@gmail.com>
2023-12-17 16:49:46 -0700

    Update configuration and layout files

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,40 @@
 <script>
-    import "../app.css";
-  </script>
+  import "../app.css";
   
-  <slot />
+  let mobileMenu = false;
+
+  function toggleMobileMenu() {
+    mobileMenu = !mobileMenu;
+  }
+</script>
+
+<nav class="container mx-auto flex flex-col md:flex-row grow justify-between content-center px-4 py-4">
+  <div class="flex flex-row justify-between content-center">
+    <img src="no-code-atlas-logo.svg" alt="Logo" class="h-6">
+    <img src="Menu Icon.svg" alt="Menu Button" class="md:hidden" on:click={toggleMobileMenu}>
+  </div>
+  <ul class="hidden md:flex md:flex-row">
+    <a href="#"><li class="p-4 rounded-lg text-center">Tools</li></a>
+    <a href="#"><li class="p-4 rounded-lg text-center">Solutions</li></a>
+    <a href="#"><li class="p-4 rounded-lg text-center">Learn</li></a>
+    <a href="#"><li class="p-4 rounded-lg text-center">Resources</li></a>
+  </ul>
+  <uL class="hidden md:flex md:flex-row md:gap-1">
+    <a href="#"><li class="bg-lime-200 text-lime-900 p-4 rounded-lg text-center">Sign In</li></a>
+    <a href="#"><li class="bg-slate-200 text-slate-900 p-4 rounded-lg text-center">Sign Up</li></a>
+  </uL>
+  {#if mobileMenu}
+  <ul class="flex flex-col gap-2 py-4 md:hidden">
+    <a href="#"><li class="p-4 rounded-lg text-center">Tools</li></a>
+    <a href="#"><li class="p-4 rounded-lg text-center">Solutions</li></a>
+    <a href="#"><li class="p-4 rounded-lg text-center">Learn</li></a>
+    <a href="#"><li class="p-4 rounded-lg text-center">Resources</li></a>
+  </ul>
+  <uL class="flex flex-col gap-2 py-4 md:hidden">
+    <a href="#"><li class="bg-lime-200 p-4 text-lime-900 rounded-lg text-center">Sign In</li></a>
+    <a href="#"><li class="bg-slate-200 p-4 text-slate-900 rounded-lg text-center">Sign Up</li></a>
+  </uL>
+  {/if}
+</nav>
+
+<slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,13 @@
-<div>
-    <div>
-        <h1 class="text-red-500">Craft Your Future with No Code</h1>
+<div class="container mx-auto p-10 flex flex-col gap-20">
+    <div class="flex flex-col gap-4">
+        <h1 class="text-slate-900 text-5xl font-bold">Craft Your Future with No Code</h1>
         <p>Harness the power of No Code Atlas, your key to creating sophisticated tools, building prosperous businesses, and enhancing your everyday life. Experience the brilliance of no code and revolutionize your world!</p>
-        <a href="#"><p>Test</p></a>
+        <a href="#"><p class="px-4 py-2.5">Test</p></a>
         <a href="#"><p>Test</p></a>
     </div>
+    <img src="Header Image - Mobile.png" alt="No Code Builder Image">
 </div>
-<div>
+<div class="container mx-auto">
     <div>
         <div>
             <p>Compass</p>
@@ -14,5 +15,8 @@
         <h2>Discover the Ideal Tools for Your Vision</h2>
         <p>Your ultimate guide to discovering the perfect tools for your unique needs. Our intuitive and comprehensive tool finder will match you with the ideal resources to build innovative solutions, launch successful businesses, and streamline your life.</p>
         <a href="#"><p>Test</p></a>
+    </div>
+    <div>
+        <img src="#" alt="Compass">
     </div>
 </div>
