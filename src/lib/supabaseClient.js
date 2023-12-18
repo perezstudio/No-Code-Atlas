4bd1606ba717a5aba08a0e2c3c1a0147a76b5f32
Kevin Perez <kp952547@gmail.com>
2023-12-17 20:17:53 -0700

    Update Supabase client configuration

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,3 +1,4 @@
 import { createClient } from '@supabase/supabase-js'
+import { PUBLIC_PROJECT_ID, PUBLIC_ANON_KEY } from '$env/dynamic/public'
 
-export const supabase = createClient('https://<project>.supabase.co', '<your-anon-key>')
+export const supabase = createClient('https://$(PUBLIC_PROJECT_ID).supabase.co', '$(PUBLIC_ANON_KEY)')
