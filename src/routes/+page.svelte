c4dc8dd5dd705d4285421ed3c4a192334190cd7d
Kevin Perez <kp952547@gmail.com>
2023-12-17 20:03:14 -0700

    Add Supabase dependencies and update page layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,16 +9,19 @@
     </div>
     <img src="Header Image - Mobile.png" alt="No Code Builder Image">
 </div>
-<div class="container mx-auto">
-    <div>
-        <div>
-            <p>Compass</p>
+<div class="container mx-auto px-5 py-5 md:px-8 lg:px-10">
+    <div class="flex flex-col gap-16 p-8 bg-slate-100 rounded-xl">
+        <div class="flex flex-col gap-4">
+            <div class="flex flex-row gap-2 items-center">
+                <img src="Compass Icon.svg" alt="Compass">
+                <p class="text-lime-500">Compass</p>
+            </div>
+            <h2 class="text-slate-900 text-4xl font-bold">Discover the Ideal Tools for Your Vision</h2>
+            <p class="text-slate-600">Your ultimate guide to discovering the perfect tools for your unique needs. Our intuitive and comprehensive tool finder will match you with the ideal resources to build innovative solutions, launch successful businesses, and streamline your life.</p>
+            <a href="#"><p class="px-4 py-2.5 bg-lime-200 rounded-lg text-center">Find My Stack</p></a>
         </div>
-        <h2>Discover the Ideal Tools for Your Vision</h2>
-        <p>Your ultimate guide to discovering the perfect tools for your unique needs. Our intuitive and comprehensive tool finder will match you with the ideal resources to build innovative solutions, launch successful businesses, and streamline your life.</p>
-        <a href="#"><p>Test</p></a>
-    </div>
-    <div>
-        <img src="#" alt="Compass">
+        <div>
+            <img src="#" alt="Compass">
+        </div>
     </div>
 </div>
